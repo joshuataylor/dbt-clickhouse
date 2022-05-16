b1337950dc49de5588b7fcca71f8954a5bbdd412
Josh Taylor <joshuataylorx@gmail.com>
2022-05-16 19:37:07 +0800

    hack together dbt 1.0.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ package_name = 'dbt-clickhouse'
 package_version = _dbt_clickhouse_version()
 description = '''The Clickhouse plugin for dbt (data build tool)'''
 
-dbt_version = '1.0.4'
+dbt_version = '1.1.0'
 
 if not package_version.startswith(dbt_version):
     raise ValueError(
@@ -54,7 +54,7 @@ setup(
     },
     install_requires=[
         f'dbt-core=={dbt_version}',
-        'clickhouse-driver>=0.2.2',
+        'clickhouse-driver>=0.2.3',
     ],
     python_requires=">=3.6",
     platforms='any',
